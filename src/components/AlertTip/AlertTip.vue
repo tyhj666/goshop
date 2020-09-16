a861e54e9fa86674621403c2115fe899caeb02f6
tyhj666 <2390577531@qq.com>
2020-09-16 14:16:01 +0800

    添加登录相关的代码以及提升组件

---
+++ src/components/AlertTip/AlertTip.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="alert_container">
+        <section class="tip_text_container">
+            <div class="tip_icon">
+                <span></span>
+                <span></span>
+            </div>
+            <p class="tip_text">alerText</p>
+            <div class="confrim" @click="closeTip">确认</div>
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        alertText:String
+    },
+    methods:{
+        closeTip(){
+            // 分发自定义事件(closeTip)
+            this.$emit('closeTip')
+        }
+    }
+}
+</script>
+<script>
+export default {
+    
+}
+</script>
--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -6,7 +6,38 @@
 </template>
 <script>
 export default {
-    
+    methods:{
+        getCode(){
+            // 如果当前没有计时，启动倒计时
+            if (!this.computeTime) {
+               this.computeTime=30
+          const intervalId =  setInterval(() => {
+               this.computeTime--
+               if (this.computeTime<=0) {
+                    clearInterval(intervalId)
+               }
+            },1000)  
+             // 发送异步请求
+            }
+           
+
+           
+        },
+        // 异步登录
+        login(){
+        // 前台表单验证
+        if (this.loginWay) {// 短信登录
+            const {rightPhone, phone, code} = this
+            if (!this.rightPhone) {
+                //手机号不正确
+            }else if (!code) {
+                //
+            }
+        }else { // 密码登录
+            const {name, pwd, captcha}=this
+        }
+        }
+    }
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
